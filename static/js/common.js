73dc9766c7c3a661c245d6dc2435a05f70a94824
iticus <ticus.ionut@gmail.com>
2018-07-26 15:49:15 +0300

    fixed pitch value and removed yaw from calculation

--- static/js/common.js
+++ static/js/common.js
@@ -96,9 +96,9 @@ function toRadians(angle) {
 function getNewPositionRPY(lng, lat, alt, dist, roll, pitch, yaw) {
 	//https://stackoverflow.com/questions/33984877/given-point-a-angles-and-length-to-point-b-calculate-point-b
 	var projCoords = proj4(srcProj, dstProj, [lng, lat]);
-	var x1 = projCoords[0] + dist * Math.cos(toRadians(yaw)) * Math.sin(toRadians(pitch));
-	var y1 = projCoords[1] + dist * Math.sin(toRadians(yaw));
-	var z1 = alt + dist * Math.cos(toRadians(yaw)) * Math.cos(toRadians(pitch));
+	var x1 = projCoords[0] + dist * Math.cos(toRadians(roll)) * Math.sin(toRadians(90-pitch));
+	var y1 = projCoords[1] + dist * Math.sin(toRadians(roll));
+	var z1 = alt + dist * Math.cos(toRadians(roll)) * Math.cos(toRadians(90-pitch));
 	projCoords = proj4(dstProj, srcProj, [x1, y1]);
 	return [projCoords[0], projCoords[1], z1];
 }
