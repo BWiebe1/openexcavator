037bf0397bbdfb2a312063dab95b3c1aafeab463
iticus <ticus.ionut@gmail.com>
2019-08-03 01:18:03 +0300

    added export functionality for tools page

--- openexcavator/static/js/tools.js
+++ openexcavator/static/js/tools.js
@@ -5,7 +5,6 @@ let currentData = null;
 let startPosition = null;
 let startData = null;
 let polyline = null;
-let antennaHeight = null;
 
 function initMap() {
 	myMap = L.map('mapid').setView([53.58442963725551, -110.51799774169922], 18);
@@ -31,6 +30,16 @@ function initMap() {
 	myMap.on('click', onMapClick);
 	myMap.invalidateSize();
 	refreshPosition();
+}
+
+function download(filename, text) {
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 }
 
 function setValuesData(prefix, data) {
@@ -73,7 +82,7 @@ function refreshPosition() {
 					utmZone.num = aux.zoneNum;
 					utmZone.letter = aux.zoneLetter;
 				}
-				let projStartCoords = fromLatLon(startData.lat, startData.lng, utmZone.num);startData.alt;
+				let projStartCoords = fromLatLon(startData.lat, startData.lng, utmZone.num);
 				let projCoords = fromLatLon(data.lat, data.lng, utmZone.num);
 				let run = pointToPointDistance(projStartCoords.easting, projStartCoords.northing, 0, projCoords.easting, projCoords.northing, 0);
 				let rise = data.alt - startData.alt;
@@ -107,11 +116,24 @@ function refreshPosition() {
 }
 
 $(document).ready(function() {
-	antennaHeight = parseFloat($('#antenna_height').text());
+	//antennaHeight = parseFloat($('#antenna_height').text());
 	if (sessionStorage.getItem("startData") !== null) {
 		startData = JSON.parse(sessionStorage.getItem("startData"));
 		setValuesData('start_', startData);
 	}
+	$('#export').click(function(){
+		let document = {
+            "type": "FeatureCollection",
+            "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}},
+            "features": [
+				{"type": "Feature", "properties": {"name": "start", "solution status": startData.fix},
+					"geometry": {"type": "Point", "coordinates": [startData.lng, startData.lat, startData.alt]}},
+				{"type": "Feature", "properties": {"name": "stop", "solution status": currentData.fix },
+					"geometry": {"type": "Point", "coordinates": [currentData.lng, currentData.lat, currentData.alt]}}
+            ]
+		};
+		download("export.geojson", JSON.stringify(document));
+	});
 	$('#mark').click(function(){
 		startData = JSON.parse(JSON.stringify(currentData)); //"deep" copy for simple data
 		sessionStorage.setItem('startData', JSON.stringify(startData));
